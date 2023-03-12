34663fdf3f96644df99adac878b70be08e0276aa
Andresha <lakshshaw570@gmail.com>
2023-03-12 11:35:22 +0530

    added bg-col

--- src/Screen2/Cards.js
+++ src/Screen2/Cards.js
@@ -1,17 +1,45 @@
 import "../Cards.css";
 import { AiOutlineDelete } from "react-icons/ai";
+import { useState } from "react";
+
 function Cards(props) {
   const inputTxt = props.newData;
   const remeoveSpaceText = inputTxt.replace(/\s/g, "");
+  const [filterChar, setFilterChar] = useState("");
+
 
   const data = Array.from(remeoveSpaceText);
+  data.forEach((ele, index) => {
+   const a=data.indexOf(ele);
+    if (index !== a) {
+      console.log(ele)
+    }
+  });
+
+  const values=['a','h','a','r','a'];
+  // -----------reference------------
+  //  const values=['a','a','h','a','r','a'];
+  // data.map((val)=>{
+  // if(values.includes(val)){
+  //   console.log(val)
+  // }else{
+  //   console.log("notmatched")
+  // }
+  // })
+
+
+  const handelDel=()=>{
+    console.log("clicked")
+  }
   return (
     <div className="CardWrapper">
       {data.map((value, index) => {
         return (
           <div className="cards" key={index}>
-            <p className="cardVal">{value}</p>
-            <AiOutlineDelete color="red" size={20} />
+            {values.includes(value)?(
+              <p style={{backgroundColor:'blue',color:"white"}} className="cardVal">{value}</p>
+            ):<p style={{backgroundColor:'red',color:"white"}} className="cardVal">{value}</p>}
+            <AiOutlineDelete color="red" size={20} onClick={handelDel}/>
           </div>
         );
       })}
